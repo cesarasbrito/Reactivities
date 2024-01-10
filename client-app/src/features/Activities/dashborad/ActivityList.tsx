60fd2af1bc5c5f3431045422db501f8cf4363e45
Cesar Brito <cesarasbrito@gmail.com>
2024-01-10 10:05:26 -0300

    final sessao 5

--- client-app/src/features/Activities/dashborad/ActivityList.tsx
+++ client-app/src/features/Activities/dashborad/ActivityList.tsx
@@ -4,9 +4,11 @@ import { Button, Item, Label, Segment } from "semantic-ui-react";
 
 interface Props{
     activities: Activity[];
+    selectActivity: (id: string) => void;
+    deleteActivity: (id: string) => void;
 }
 
-export default function ActivityList({activities}: Props){
+export default function ActivityList({activities, selectActivity, deleteActivity}: Props){
     return (
         <Segment>
             <Item.Group divided>
@@ -24,7 +26,8 @@ export default function ActivityList({activities}: Props){
                                 <div>{activity.city}, {activity.venue}</div>
                             </Item.Description>
                             <Item.Extra>
-                                <Button floated="right" content="View" color="blue"></Button>
+                                <Button onClick={() => selectActivity(activity.id)} floated="right" content="View" color="blue"></Button>
+                                <Button onClick={() => deleteActivity(activity.id)} floated="right" content="Delete" color="red"></Button>
                                 <Label basic content={activity.category}></Label>
                             </Item.Extra>
                         </Item.Content>
